1ce50a0caa3c8246d1546a7759dac52852c17608
BrunoMihaljec <75934875+BrunoMihaljec@users.noreply.github.com>
2020-12-20 13:12:27 +0100

    First commit

--- consoleBasicCalculator/console Basic Calculator Upgrade/Properties/AssemblyInfo.cs
+++ consoleBasicCalculator/console Basic Calculator Upgrade/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Drugi Zadatak")]
+[assembly: AssemblyTitle("consoleBasicCalculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Drugi Zadatak")]
+[assembly: AssemblyProduct("consoleBasicCalculator")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- consoleBasicCalculator/console BasicCalculator/Properties/AssemblyInfo.cs
+++ consoleBasicCalculator/console BasicCalculator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZadaciSelenium")]
+[assembly: AssemblyTitle("consoleBasicCalculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZadaciSelenium")]
+[assembly: AssemblyProduct("consoleBasicCalculator")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
